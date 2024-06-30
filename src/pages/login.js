07771bea095f1d58aac040e363fc1a767324e230
ynghan <119781387+ynghan@users.noreply.github.com>
2024-07-01 02:18:34 +0900

    refactor #4(login): login.js 페이지 수정
    - axios를 통한 post 기능 추가
    - radio 버튼 생성

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,19 +1,38 @@
-import "./css/login.css"
+import "./css/login.css";
+import axios from 'axios';
+
 
 function Login(props) {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const userEmail = form.userEmail.value;
+        const userPassword = form.userPassword.value;
+
+        try {
+            const response = await axios.post('/api/v1/login', {
+                email: userEmail,
+                password: userPassword
+            });
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
-        <div style={{marginTop: '55px'}}>
+        <div style={{display: "flex", justifyContent: "center"}}>
             <div className="login-wrapper">
             <h2>Login</h2>
-            <form method="post" action="서버의url" id="login-form">
+            <form method="post" onSubmit={handleSubmit} id="login-form">
                 <input type="text" name="userEmail" placeholder="Email"/>
                 <input type="password" name="userPassword" placeholder="Password"/>
                 <input type="submit" value="Login"/>
             </form>
             </div>
         </div>
-
     )
 }
+
 
 export default Login;
