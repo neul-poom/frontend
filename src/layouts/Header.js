653330ae6f76a780cc29054f58f00d00359f8e56
ynghan <119781387+ynghan@users.noreply.github.com>
2024-06-30 22:13:12 +0900

    refactor #15(Layout): Layout.js 페이지 수정
    - Header.js에서 useNavigate 기능 추가
    - Layout.js에서 flex 적용 후 Header Body Footer 비율 조정

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,17 +1,21 @@
 import { Navbar, Nav, Button, Container } from "react-bootstrap"
+import { useNavigate } from "react-router-dom";
 
 
 function Header() {
+
+  let navigate = useNavigate();
+
     return (
         <>
-          <Navbar className="header" bg="light" data-bs-theme="light">
+          <Navbar className="header" bg="#39A7FF" data-bs-theme="light">
             <Container>
-              <Navbar.Brand href="#home">NeulPoom</Navbar.Brand>
+              <Navbar.Brand onClick={() => { navigate('/')}}>NeulPoom</Navbar.Brand>
               <Nav className="ms-auto">
-                <Button variant="light">마이페이지</Button>{' '}
-                <Button variant="light">장바구니</Button>{' '}
-                <Button variant="light">로그인</Button>{' '}
-                <Button variant="light">회원가입</Button>{' '}
+                <Nav.Link variant="light" onClick={() => { navigate('/mypage')}}>마이페이지</Nav.Link>
+                <Nav.Link variant="light" onClick={() => { navigate('/bucket')}}>장바구니</Nav.Link>
+                <Nav.Link variant="light" onClick={() => { navigate('/login')}}>로그인</Nav.Link>
+                <Nav.Link variant="light" onClick={() => { navigate('/signup')}}>회원가입</Nav.Link>
               </Nav>
             </Container>
           </Navbar>
--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -4,11 +4,17 @@ import React from 'react'
 
 function Layout(props) {
     return (
-        <>
-        <Header/>
-            {props.children}
-        <Footer/>
-        </>
+        <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+            <div style={{ flex: '3 0 55px'}}>
+                <Header/>
+            </div>
+            <div style={{ flex: '6 0 auto'}}>
+                {props.children}
+            </div>
+            <div style={{ flex: '1 0 41px'}}>
+                <Footer/>
+            </div>
+        </div>
     )
 }
 
