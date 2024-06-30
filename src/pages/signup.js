5e802edf32ff438a7ff6313d1b4d08086cb4c7b3
ynghan <119781387+ynghan@users.noreply.github.com>
2024-07-01 02:17:30 +0900

    refactor #5(signup): signup.js 페이지 수정
    - axios를 통한 post 기능 추가
    - radio 버튼 생성

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,19 +1,45 @@
 import "./css/signup.css"
+import axios from 'axios';
 
 
 function Signup(props) {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const userEmail = form.userEmail.value;
+        const userPassword = form.userPassword.value;
+        const userName = form.userName.value;
+        const userRole = form.userRole.value;
+
+        try {
+            const response = await axios.post('/api/v1/signup', {
+                email: userEmail,
+                password: userPassword,
+                username: userName,
+                role: userRole
+            });
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
-        <>
+        <div style={{display: "flex", justifyContent: "center"}}>
             <div class="sign-wrapper">
             <h2>Signup</h2>
-            <form method="post" action="서버의url" id="sign-form">
+            <form method="post" onSubmit={handleSubmit} id="sign-form">
                 <input type="text" name="userEmail" placeholder="Email"/>
                 <input type="password" name="userPassword" placeholder="Password"/>
                 <input type="text" name="userName" placeholder="Username"/>
+                <div id="radio">
+                        <input type="radio" name="userRole" value="1" checked/>Student
+                        <input type="radio" name="userRole" value="2"/>Teacher
+                </div>
                 <input type="submit" value="Signup"/>
             </form>
             </div>
-        </>
+        </div>
     )
 }
 
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
         createProxyMiddleware({
-            target: 'http://localhost:8080', // 서버 URL or localhost:설정한포트번호
+            target: 'http://localhost:8082', // 서버 URL or localhost:설정한포트번호
             changeOrigin: true,
             pathFilter: '/api',
         }),
