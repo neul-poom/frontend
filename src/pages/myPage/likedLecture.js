b866b815ea6ebabbb0a1ce54d98e068b9847cae6
ynghan <119781387+ynghan@users.noreply.github.com>
2024-07-09 22:49:02 +0900

    feat #8 (likedLecture): likedLecture 페이지 생성
    - likedLecture.js 페이지 생성

---
+++ src/pages/myPage/likedLecture.js
@@ -0,0 +1,42 @@
+import SideNav from "../../components/SideNav";
+import '../css/myPage/lecture.css';
+
+// import ItemList from "../css/../components/ItemList";
+
+
+
+function LikedLecture() {
+    return (
+        <div className="mypage-layout">
+            <SideNav/>
+            <div className="mypage-content">
+                <div className="user-info">
+                    <img className="thumbnail-image" src="thumbnail.jpg" alt="썸네일" />
+                    <div className="lecture-details">
+                        <div>강의 제목</div>
+                        <div>강사 이름</div>
+                        <div>가격</div>
+                    </div>
+                </div>
+                <div className="user-info">
+                    <img className="thumbnail-image" src="thumbnail.jpg" alt="썸네일" />
+                    <div className="lecture-details">
+                        <div>강의 제목</div>
+                        <div>강사 이름</div>
+                        <div>가격</div>
+                    </div>
+                </div>
+                <div className="user-info">
+                    <img className="thumbnail-image" src="thumbnail.jpg" alt="썸네일" />
+                    <div className="lecture-details">
+                        <div>강의 제목</div>
+                        <div>강사 이름</div>
+                        <div>가격</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LikedLecture;
