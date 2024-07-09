1f4ca4e6d7f8291f14867ea7883692eed9a8e3e7
ynghan <119781387+ynghan@users.noreply.github.com>
2024-07-09 23:01:49 +0900

    feat #16 (layout): itemList 페이지 생성
    - itemList.js 페이지 생성
    - itemList.css 페이지 생성

---
+++ src/components/ItemList.js
@@ -0,0 +1,19 @@
+import './css/ItemList.css'
+
+function ItemList() {
+    return (
+        <div className="image-container">
+                {/* <div className="self-container"><img className="image" src={"https://image.msscdn.net/images/goods_img/20240422/4074673/4074673_17146129412760_500.jpg"}/></div> */}
+                <div className="self-container"><img className="image" src={"https://image.msscdn.net/images/goods_img/20230206/3062046/3062046_17194520418916_320.jpg"}/></div>
+                <div className="self-container"><img className="image" src={"https://image.msscdn.net/images/goods_img/20240422/4074673/4074673_17146129412760_500.jpg"}/></div>
+                <div className="self-container"><img className="image" src={"https://image.msscdn.net/images/goods_img/20230313/3142574/3142574_16824927242761_320.jpg"}/></div>
+                <div className="self-container"><img className="image" src={"https://image.msscdn.net/images/goods_img/20191112/1220731/1220731_17073700380718_500.jpg"}/></div>
+                <div className="self-container"><img className="image" src={"https://image.msscdn.net/images/goods_img/20230830/3512452/3512452_17193894748781_320.jpg"}/></div>
+                <div className="self-container"><img className="image" src={"https://image.msscdn.net/images/goods_img/20240422/4074673/4074673_17146129412760_500.jpg"}/></div>
+                <div className="self-container"><img className="image" src={"https://image.msscdn.net/images/goods_img/20240422/4074673/4074673_17146129412760_500.jpg"}/></div>
+                <div className="self-container"><img className="image" src={"https://image.msscdn.net/images/goods_img/20240422/4074673/4074673_17146129412760_500.jpg"}/></div>
+        </div>
+    )
+}
+
+export default ItemList;
